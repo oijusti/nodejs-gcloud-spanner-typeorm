9792e5d285d83e557f683891b9fb55d03b0b4ac6
Oscar Justi <oijusti@gmail.com>
2025-02-13 21:34:01 -0500

    create custom migrations

--- runme.sh
+++ runme.sh
@@ -27,12 +27,27 @@ export SPANNER_EMULATOR_HOST=localhost:9010
 # export GOOGLE_APPLICATION_CREDENTIALS="~/path/to/your/credentials.json"
 
 # Run npm commands
-npm i
+# npm i
 # Setup database settings inside data-source.ts file if needed
-npm start
+# npm start
 
 # Testing Spanner setup
 # gcloud spanner instances list
 # gcloud spanner databases list --instance=test-instance
 # gcloud spanner databases ddl describe test-db --instance=test-instance
 # gcloud spanner databases execute-sql test-db --instance=test-instance --sql="SELECT * FROM User"
+gcloud spanner databases ddl update test-db --instance=test-instance --ddl="
+  CREATE SEQUENCE custom_migration_id_sequence OPTIONS (
+    sequence_kind = 'bit_reversed_positive',
+    skip_range_min = 1,
+    skip_range_max = 4294967296                     
+  );"
+gcloud spanner databases ddl update test-db --instance=test-instance --ddl="
+  CREATE TABLE custom_migrations (
+    id INT64 DEFAULT (GET_NEXT_SEQUENCE_VALUE(SEQUENCE custom_migration_id_sequence)),
+    timestamp INT64 NOT NULL,
+    name STRING(MAX) NOT NULL
+  ) PRIMARY KEY (id);"
+
+# $ ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate ./src/migrations/anyname -d ./src/data-source.ts
+# $ ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:run -d ./src/data-source.ts
--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,28 @@ const appDataSourceInitialize = async () => {
   await AppDataSource.initialize();
 
   console.log("Running migrations...");
-  const migrationExecutor = new SpannerMigrationExecutor(AppDataSource);
-  await migrationExecutor.executePendingMigrations();
+  await AppDataSource.runMigrations();
 
-  console.log("Inserting a new user into the database...");
-  const user = new User();
-  user.id = uuidv4();
-  user.firstName = "John";
-  user.lastName = "Doe";
-  user.age = 25;
-  user.createdAt = new Date();
-  await AppDataSource.manager.save(user);
+  // console.log("Running migrations...");
+  // const migrationExecutor = new SpannerMigrationExecutor(AppDataSource);
+  // await migrationExecutor.executePendingMigrations();
 
-  console.log("Loading users from the database...");
-  const users = await AppDataSource.manager.find(User);
-  console.log("Loaded users: ", users);
+  // console.log("Running migrations...");
+  // const migrationExecutor = new SpannerMigrationExecutor(AppDataSource);
+  // await migrationExecutor.executePendingMigrations();
+
+  // console.log("Inserting a new user into the database...");
+  // const user = new User();
+  // user.id = uuidv4();
+  // user.firstName = "John";
+  // user.lastName = "Doe";
+  // user.age = 25;
+  // user.createdAt = new Date();
+  // await AppDataSource.manager.save(user);
+
+  // console.log("Loading users from the database...");
+  // const users = await AppDataSource.manager.find(User);
+  // console.log("Loaded users: ", users);
 
   console.log(
     "Here you can setup and run express / fastify / any other framework."
